e52ff0527ac0908388d1ede7676217a2861e63c5
elmø <anon_fwd@icloud.com>
2022-06-06 14:52:27 +0000

    fix typo and cleanup .env

--- scripts/switch_to_https.sh
+++ scripts/switch_to_https.sh
@@ -3,7 +3,7 @@ set -o errexit
 
 if [[ -f /opt/seafile/conf/seahub_settings.py ]]; then
     echo "Changing http to https in seahub_settings.py"
-    sed -i -e 's#http://#https://#g' /opt/seafile/conf/seahub_settings.p
+    sed -i -e 's#http://#https://#g' /opt/seafile/conf/seahub_settings.py
     /opt/seafile/seafile-server-latest/seahub.sh restart
 else 
     echo "File not found: /opt/seafile/conf/seahub_settings.py. Exiting.."
